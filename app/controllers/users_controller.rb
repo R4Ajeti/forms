260f9bde304dd692d4d6a588512a4460bd049948
Rinor Ajeti <r4ajeti@gmail.com>
2019-10-25 22:50:18 +0200

    UPDATED: HTML Form!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,18 @@ class UsersController < ApplicationController
     def index
     end
     def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        @user = User.new(post_params)
 
-    if @user.save
-        redirect_to new_user_path
-    else
-        render :new
+        if @user.save
+            redirect_to new_user_path
+        else
+            render :new
+        end
     end
+
+    private
+
+    def post_params
+        params.require(:user).permit(:username, :email, :password)
     end
 end
